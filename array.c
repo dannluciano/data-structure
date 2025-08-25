57860beec9bd6418502cad6ab7ccb5c9db7e6b0e
Dann Luciano <dannluciano@gmail.com>
2025-08-24 21:56:46 -0300

    Add Array

---
+++ array.c
@@ -0,0 +1,314 @@
+#include "array.h"
+
+static inline int *int_alloc_init(int valor)
+{
+    int *p = malloc(sizeof(int));
+    *p = valor;
+    return p;
+}
+
+static inline long *long_alloc_init(long valor)
+{
+    long *p = malloc(sizeof(long));
+    *p = valor;
+    return p;
+}
+
+static inline float *float_alloc_init(float valor)
+{
+    float *p = malloc(sizeof(float));
+    *p = valor;
+    return p;
+}
+
+static inline char *string_alloc_init(const char *str)
+{
+    if (!str)
+        return NULL;
+    char *p = malloc(strlen(str) + 1);
+    strcpy(p, str);
+    return p;
+}
+
+object_t *obj_alloc_init(void *value, type_t type)
+{
+    object_t *self = (object_t *)malloc(sizeof(object_t));
+    if (self == NULL)
+    {
+        perror("Memory Error");
+        exit(EXIT_FAILURE);
+    }
+
+    self->value = value;
+    self->type = type;
+    return self;
+}
+
+void obj_free(object_t *self)
+{
+    if (!self)
+        return;
+    switch (self->type)
+    {
+    case TYPE_STRING:
+    case TYPE_INT:
+    case TYPE_LONG:
+    case TYPE_FLOAT:
+        free(self->value);
+        break;
+    default:
+        break;
+    }
+    free(self);
+}
+void obj_print(object_t *self)
+{
+    if (self != NULL)
+    {
+
+        switch (self->type)
+        {
+        case TYPE_INT:
+            printf("%d", *(int *)self->value);
+            break;
+        case TYPE_LONG:
+            printf("%ld", *(long *)self->value);
+            break;
+        case TYPE_FLOAT:
+            printf("%.2f", *(float *)self->value);
+            break;
+        case TYPE_STRING:
+            printf("\"%s\"", (char *)self->value);
+            break;
+        default:
+            printf("Desconhecido");
+        }
+    }
+    else
+    {
+        printf("NULL");
+    }
+}
+
+array_t *array_alloc_init(unsigned int capacity)
+{
+    struct array_t *array = (struct array_t *)malloc(sizeof(struct array_t));
+    if (array == NULL)
+    {
+        perror("Memory Error");
+        exit(EXIT_FAILURE);
+    }
+
+    if (capacity > 0)
+    {
+        array->elements = (struct object_t **)calloc(sizeof(struct object_t), capacity);
+    }
+    else
+    {
+        array->elements = NULL;
+    }
+    array->capacity = capacity;
+    array->size = 0;
+
+    return array;
+}
+
+void array_free(array_t *self)
+{
+    if (self->elements)
+    {
+        free(self->elements);
+    }
+    free(self);
+}
+
+void array_append(array_t *self, void *value, type_t type)
+{
+    object_t *obj = obj_alloc_init(value, type);
+    if (self->capacity == 0)
+    {
+        printf("Reallocation not implemented\n");
+    }
+    if (self->size < self->capacity)
+    {
+        self->elements[self->size] = obj;
+        self->size = self->size + 1;
+    }
+    else
+    {
+        printf("Reallocation not implemented\n");
+    }
+}
+
+void array_insert(array_t *self, unsigned int position, void *value, type_t type)
+{
+    if (!self)
+        return;
+    if (self->size < self->capacity)
+    {
+        if (position > self->size)
+        {
+            array_append(self, value, type);
+            return;
+        }
+
+        object_t *obj = obj_alloc_init(value, type);
+
+        for (size_t i = self->size; i > position; i--)
+        {
+            self->elements[i] = self->elements[i - 1];
+        }
+
+        self->elements[position] = obj;
+        self->size = self->size + 1;
+    }
+    else
+    {
+        printf("Reallocation not implemented\n");
+    }
+}
+
+unsigned int array_size(array_t *self)
+{
+    return self->size;
+}
+
+unsigned int array_capacity(array_t *self)
+{
+    return self->capacity;
+}
+
+object_t *array_at(array_t *self, unsigned int idx)
+{
+    if (!self || idx < 0 || idx >= self->size)
+        return NULL;
+    return self->elements[idx];
+}
+
+void array_print(array_t *self)
+{
+    printf("[");
+    for (size_t i = 0; i < self->size; i++)
+    {
+        obj_print(self->elements[i]);
+        if (i < self->size - 1)
+        {
+            printf(", ");
+        }
+    }
+
+    printf("] (size: %u, capacity: %u)\n", self->size, self->capacity);
+}
+
+void array_remove_at(array_t *self, unsigned int idx)
+{
+    if (!self || self->size == 0 || idx >= self->size)
+        return;
+
+    for (size_t i = idx; i < self->size; i++)
+    {
+        self->elements[i] = self->elements[i + 1];
+    }
+    self->size = self->size - 1;
+}
+
+void array_foreach(array_t *self, void (*callback)(object_t *obj))
+{
+    if (!self || !callback)
+        return;
+    for (size_t i = 0; i < self->size; i++)
+    {
+        callback(self->elements[i]);
+    }
+}
+
+void array_foreach_ctx(array_t *self, void (*callback)(object_t *obj, void *ctx), void *ctx)
+{
+    if (!self || !callback)
+        return;
+
+    for (size_t i = 0; i < self->size; i++)
+    {
+        callback(self->elements[i], ctx);
+    }
+}
+
+void print_cb(object_t *obj)
+{
+    obj_print(obj);
+    printf("->");
+}
+
+int main(const int argc, char **argv)
+{
+    puts("===================================================================");
+    struct array_t *array0 = array_alloc_init(0);
+    array_append(array0, int_alloc_init(10), TYPE_INT);
+    array_free(array0);
+    puts("===================================================================");
+    struct array_t *array1 = array_alloc_init(3);
+    array_print(array1);
+    array_append(array1, int_alloc_init(10), TYPE_INT);
+    array_append(array1, int_alloc_init(20), TYPE_INT);
+    array_append(array1, int_alloc_init(30), TYPE_INT);
+    array_insert(array1, 0, int_alloc_init(4), TYPE_INT);
+    array_insert(array1, 5, int_alloc_init(5), TYPE_INT);
+    array_print(array1);
+    puts("===================================================================");
+    struct array_t *array2 = array_alloc_init(10);
+    array_print(array2);
+    array_insert(array2, 0, int_alloc_init(2), TYPE_INT);
+    array_insert(array2, 0, int_alloc_init(1), TYPE_INT);
+    array_insert(array2, 3, int_alloc_init(4), TYPE_INT);
+    array_insert(array2, 1, int_alloc_init(6), TYPE_INT);
+    array_print(array2);
+
+    puts("===================================================================");
+    struct array_t *array3 = array_alloc_init(10);
+    array_print(array3);
+    array_insert(array3, 0, int_alloc_init(1), TYPE_INT);
+    array_insert(array3, 1, float_alloc_init(3.141597), TYPE_FLOAT);
+    array_insert(array3, 2, long_alloc_init(24294967297), TYPE_LONG);
+    array_insert(array3, 3, string_alloc_init("IFPI"), TYPE_STRING);
+    array_print(array3);
+
+    puts("===================================================================");
+    int position = 0;
+    object_t *num = array_at(array3, position);
+    obj_print(num);
+    putchar('\n');
+
+    object_t *obj = obj_alloc_init("T", TYPE_STRING);
+    obj_print(obj);
+    putchar('\n');
+
+    puts("===================================================================");
+    array_foreach(array1, print_cb);
+    putchar('\n');
+    array_foreach(array2, print_cb);
+    putchar('\n');
+    array_foreach(array3, print_cb);
+    putchar('\n');
+
+    puts("===================================================================");
+    struct array_t *array4 = array_alloc_init(10);
+    array_remove_at(array4, 0);
+    array_remove_at(array4, 1);
+    array_print(array4);
+
+    array_remove_at(array1, 0);
+    array_remove_at(array1, 1);
+    array_remove_at(array1, array1->size - 1);
+    array_remove_at(array1, 6);
+    array_remove_at(array1, 6);
+    array_remove_at(array1, 0);
+    array_remove_at(array1, 0);
+    array_print(array1);
+
+    array_free(array1);
+    array_free(array2);
+    array_free(array3);
+    array_free(array4);
+
+    return EXIT_SUCCESS;
+}
---
+++ array.h
@@ -0,0 +1,61 @@
+#if !defined(ARRAY_T)
+#define ARRAY_T
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static inline int *int_alloc_init(int valor);
+static inline long *long_alloc_init(long valor);
+static inline float *float_alloc_init(float valor);
+static inline char *string_alloc_init(const char *str);
+
+typedef enum
+{
+    TYPE_INT,
+    TYPE_LONG,
+    TYPE_FLOAT,
+    TYPE_STRING,
+} type_t;
+
+typedef struct object_t
+{
+    void *value;
+    type_t type;
+} object_t;
+
+object_t *obj_alloc_init(void *value, type_t type);
+void obj_free(object_t *self);
+void obj_print(object_t *self);
+bool obj_equals(object_t *a, object_t *b);
+
+typedef struct array_t
+{
+    object_t **elements;
+    unsigned int size;
+    unsigned int capacity;
+} array_t;
+
+array_t *array_alloc_init(unsigned int capacity);
+void array_free(array_t *self);
+void array_append(array_t *self, void *value, type_t type);
+void array_insert(array_t *self, unsigned int position, void *value, type_t type);
+unsigned int array_size(array_t *self);
+unsigned int array_capacity(array_t *self);
+object_t *array_at(array_t *self, unsigned int idx);
+void array_print(array_t *self);
+void array_remove_at(array_t *self, unsigned int idx);
+void array_remove(array_t *self, object_t *obj);
+void array_sort(array_t *self, int (*cmp)(object_t *, object_t *));
+array_t *array_clone(array_t *self);
+bool array_equals(array_t *a, array_t *b);
+void array_foreach(array_t *self, void (*callback)(object_t *obj));
+void array_foreach_ctx(array_t *self, void (*callback)(object_t *obj, void *ctx), void *ctx);
+
+#define ARRAY_APPEND_INT(array, val) array_append((array), int_alloc_init(val), TYPE_INT)
+#define ARRAY_APPEND_LONG(array, val) array_append((array), long_alloc_init(val), TYPE_LONG)
+#define ARRAY_APPEND_FLOAT(array, val) array_append((array), float_alloc_init(val), TYPE_FLOAT)
+#define ARRAY_APPEND_STRING(array, val) array_append((array), string_alloc_init(val), TYPE_STRING)
+
+#endif // ARRAY_T
